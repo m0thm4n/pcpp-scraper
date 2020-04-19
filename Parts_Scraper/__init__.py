3283e307f2311fdb10e8ac1a0883047d48a76b48
petervuyk456 <peter.vuyk456@gmail.com>
2020-04-19 19:55:56 -0400

    Created PCParts class providing easy interface to pcpartspicker API

---
+++ Parts_Scraper/__init__.py
@@ -0,0 +1,10 @@
+from pcparts import PCParts 
+import pprint
+import json
+
+if __name__ == "__main__":
+    pp = PCParts(debug=True)
+    #pp.get_quantities()
+    #pp.get_components()
+    #pp.get_JSON()
+    
